8290f8d9b944ebec979fa6aa17369a6967c2bcae
Priyant <developerpriyant@gmail.com>
2024-01-12 09:56:09 +0530

    firstcommit

---
+++ VAC (CIA)/script.js
@@ -0,0 +1,10 @@
+console.log("Hello, I am Priyant Nikhare.")
+// alert("Happy New Year 2K24!!")
+// confirm("Are you ok?")
+// x='10' (string) or x=10 (number)
+for (let index = 0; index <= 5; index++) {    
+    console.log(index)
+}
+const pi = 3.14;
+console.log(pi)
+document.getElementById("demo").innerHTML=pi;
